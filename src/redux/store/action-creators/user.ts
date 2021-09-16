e9b1a975e0b61c47f7b62fc3e25d8925f1c77f14
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-09-16 13:29:34 +0300

    feat: sort function

--- src/redux/store/action-creators/user.ts
+++ src/redux/store/action-creators/user.ts
@@ -2,6 +2,7 @@ import axios, { AxiosResponse } from "axios";
 import { Dispatch } from "react"
 import axiosInstance from "../../../services/api";
 import { API_KEY } from "../../../ts/components/MainBlock";
+import { IUsers } from "../../../ts/types/user";
 import { IUserArticle, IUserState, UserAction, UserActionType } from "../../types/user"
 
 export const getUsers = () => {
@@ -15,13 +16,43 @@ export const getUsers = () => {
     // }
     try {
       const res = await axios.get('https://itrex-react-lab-files.s3.eu-central-1.amazonaws.com/react-test-api.json');
-      dispatch({type: UserActionType.GET_USERS, payload: res.data})
+      const result = res.data;
+      function sortByAge(arr: IUsers[]) {
+        arr.sort((a, b) => a.id > b.id ? 1 : -1);
+      }
+      sortByAge(result);
+      dispatch({type: UserActionType.GET_USERS, payload: result})
       console.log(res.data)
       dispatch({type: UserActionType.GET_USERS_SUCCESS, payload: true})
     } catch (error) {
 
     }
 
+  }
+};
+
+export const getUsersUp = () => {
+  return async (dispatch: Dispatch<UserAction>) => {
+    // try {
+    //   const res: AxiosResponse<IOKArticles> = await axiosInstance.get(
+    //     `v2/everything?q=${searchValue}&apiKey=${API_KEY}&sortBy=${sortBy}&pageSize=10&page=${page}`);
+    //   dispatch({type: UserActionType.GET_POSTS, payload: res.data.articles})
+      
+    // } catch (error) { 
+    // }
+    try {
+      const res = await axios.get('https://itrex-react-lab-files.s3.eu-central-1.amazonaws.com/react-test-api.json');
+      const result = res.data;
+      function sortByAge(arr: IUsers[]) {
+        arr.sort((a, b) => a.id < b.id ? 1 : -1);
+      }
+      sortByAge(result);
+      dispatch({type: UserActionType.GET_USERS, payload: result})
+      console.log(res.data)
+      dispatch({type: UserActionType.GET_USERS_SUCCESS, payload: true})
+    } catch (error) {
+
+    }
 
   }
 }
--- src/ts/components/MainBlock.tsx
+++ src/ts/components/MainBlock.tsx
@@ -3,6 +3,7 @@ import React, { ChangeEvent, FC, useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useTypedSelector } from '../../redux/hooks/useTypedSelector';
 import { getUsers } from '../../redux/store/action-creators/user';
+import { UserActionType } from '../../redux/types/user';
 import { IAllUsers, IUsers, SortType } from '../types/user';
 import UserInfo from './UserInfo';
 
@@ -14,25 +15,36 @@ const MainBlock: FC = () => {
   const [sortBy, setSortBy] = useState<SortType>(SortType.popularity);
   const [page, setPage] = useState<number>(1);
 
-  const [usersAll, setUsersAll] = useState<IUsers[]>([])
+  const [usersAll, setUsersAll] = useState<IUsers[]>([]);
+
+  const [sortUp, setSortUp] = useState(false);
 
   const {users, isLoad} = useTypedSelector(state => state.user);
   const dispatch = useDispatch();
 
+  async function getAllAsync() {
+    const res = await fetch('https://itrex-react-lab-files.s3.eu-central-1.amazonaws.com/react-test-api.json');
+    res.json().then(res => {
+      setUsersAll(res);
+      setIsLoading(true);
+      // console.log('object');
+      // console.log(usersAll);
+    })
+  }
+
+  const getAll = async () => {
+    const response = await axios.get('https://itrex-react-lab-files.s3.eu-central-1.amazonaws.com/react-test-api.json');
+    setUsersAll(response.data);
+    setIsLoading(true);
+    console.log('server:', usersAll);
+  };
+
   useEffect(() => {
     dispatch(getUsers());
-    // setIsLoading(true);
-    try {
-      const response = axios.get('https://itrex-react-lab-files.s3.eu-central-1.amazonaws.com/react-test-api.json').then(res => {
-        setUsersAll(res.data);
-        // setIsLoading(true);
-      });
-        // console.log('server:', response.data);
-        // setUsersAll(response.data)
-      
-    } catch (error) {
-      
-    }
+
+    // getAll();
+    // getAllAsync();
+
   }, [])
 
 
@@ -45,15 +57,22 @@ const MainBlock: FC = () => {
     event.preventDefault();
 
     // console.log(users[0].email);
+    // function sortByAge(arr: IUsers[]) {
+    //   arr.sort((a, b) => a.email < b.email ? 1 : -1);
+    // }
+    // sortByAge(users);
+    // dispatch({
+    //   type: UserActionType.GET_USERS, payload: users
+    // })
 
 
     // setIsLoading(true);
 
-    const response = await axios.get('https://itrex-react-lab-files.s3.eu-central-1.amazonaws.com/react-test-api.json');
-      console.log('server:', response.data);
-      setUsersAll(response.data);
-      setIsLoading(true);
-      console.log(users[0].email);
+    // const response = await axios.get('https://itrex-react-lab-files.s3.eu-central-1.amazonaws.com/react-test-api.json');
+    //   console.log('server:', response.data);
+    //   setUsersAll(response.data);
+    //   setIsLoading(true);
+    //   console.log(users[0].email);
       
 
     // try {
@@ -66,6 +85,37 @@ const MainBlock: FC = () => {
       //   setIsLoading(false);
       // }
   }
+
+  const sortId = () => {
+    function sortByUp(arr: IUsers[]) {
+      arr.sort((a, b) => a.id > b.id ? 1 : -1);
+    }
+    function sortByDown(arr: IUsers[]) {
+      arr.sort((a, b) => a.id < b.id ? 1 : -1);
+    }
+    if (sortUp) {
+      sortByUp(users);
+      dispatch({
+        type: UserActionType.GET_USERS, payload: users
+      });
+      setSortUp(!sortUp);
+    } else {
+      sortByDown(users);
+      dispatch({
+        type: UserActionType.GET_USERS, payload: users
+      });
+      setSortUp(!sortUp);
+    }
+    
+    // setUsersAll(usersAll);
+    // console.log(users);
+    
+  }
+
+  const handlerUser = (email: string) => {
+    console.log(email);
+  }
+
   return (
     <div className='search_wrapper'>
       <form className='search_form' onSubmit={handlerSubmit}>
@@ -82,8 +132,34 @@ const MainBlock: FC = () => {
           {isLoading ? 'Loading...' : 'Search'}
         </button>
       </form>
-      {isLoad && <div>{users[0].email}</div>}
-      {/* <UserInfo articles={users} /> */}
+      {/* {usersAll.length ? <div>{usersAll[0].email}</div>} */}
+      {isLoad ? (
+        <div>
+          <table style={{border: '1px solid black'}}>
+            <tbody>
+              <tr>
+                <td>Title</td>
+                <td>Author</td>
+                <td>
+                  Published at
+                </td>
+                <td onClick={sortId}>id</td>
+              </tr>
+              {users.map(({firstName, lastName, email, id}, index: number) => {
+                return (
+                  <tr key={index.toString()} onClick={() => handlerUser(email)}>
+                      <td>{firstName}</td>
+                      <td>{lastName}</td>
+                      <td>{email}</td>
+                      <td>{id}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+        )
+      : null}
     </div>
   );
 };
